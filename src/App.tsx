47de27d73fbe551361f41bc915be77b3c13a70f1
Harshit Dhiman <hd706020@gmail.com>
2022-05-28 14:58:21 +0530

    changed in xyz

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+function App() {
+  return (
+    <div>
+      <h1 className="bg-green-400 h-screen text-white">App changed in sfdfvdfvsdfvd text-white</h1>
+    </div>
+  );
+}
+
+export default App;
